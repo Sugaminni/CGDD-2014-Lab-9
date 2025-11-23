3590f93457d233ce62e1b64c593956a815052c32
Alex Lu <Alexhonglu@gmail.com>
2025-11-23 01:31:13 -0500

    Created animations and isGrounded

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -7,13 +7,28 @@ public class MouseController : MonoBehaviour
     public float forwardMovementSpeed = 3.0f;
 
     private Rigidbody2D playerRigidbody;
+    public Transform groundCheckTransform;
+    private bool isGrounded;
+    public LayerMask groundCheckLayerMask;
+    private Animator mouseAnimator;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
+        mouseAnimator = GetComponent<Animator>();
 
+    }
+
+    // Updates the grounded status of the player
+    void UpdateGroundedStatus()
+    {
+        //1
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+        //2
+        mouseAnimator.SetBool("isGrounded", isGrounded);
     }
 
     // Update is called once per frame
@@ -27,6 +42,6 @@ public class MouseController : MonoBehaviour
         Vector2 newVelocity = playerRigidbody.linearVelocity;
         newVelocity.x = forwardMovementSpeed;
         playerRigidbody.linearVelocity = newVelocity;
-
+        UpdateGroundedStatus();
     }
 }
